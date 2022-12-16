cfbd5032e66849e65220cb9d683dc7fec3c93808
MarkusMaal2 <markus.maal@voco.ee>
2022-12-16 17:27:25 +0200

    Add view engine and main view

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = 8415
 
+// add template engine
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get("/", (req, res) => {
-    res.send("Hello!")
+    res.render('index')
 })
 
 app.listen(port)
