5e1bbb139dbd0c8b66dd34ec4149a9dcd9c56eb8
MarkusMaal2 <markus.maal@voco.ee>
2022-12-21 14:38:34 +0200

    Add data json file and display it on a view dynamically.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+
 const app = express()
 const port = 8415
 
@@ -10,8 +11,23 @@ app.set('views', path.join(__dirname, 'views'))
 // static files s- CSS, JS, Images, etc.
 app.use(express.static(path.join(__dirname, 'public')))
 
+// use file system - r/w files
+const fs = require('fs')
+
+
 app.get("/", (req, res) => {
-    res.render('index')
+    fs.readFile('./tasks.json', 'utf-8', (err, jsonString) => {
+        if (err) {
+            console.log(`Error reading file: ${err}`)
+            return
+        }
+        try {
+            const tasks = JSON.parse(jsonString)
+            res.render('index', {tasksData : tasks})
+        } catch (err) {
+            console.log(`Error parsing file: ${err}`)
+        }
+    })
 })
 
 app.listen(port)
